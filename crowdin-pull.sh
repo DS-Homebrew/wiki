cc56bf3bcdecbdc53cd7dbb4abcc64338a02f0ba
lifehackerhansol <lifehackerhansol@ds-homebrew.com>
2024-01-17 11:19:24 -0800

    crowdin-pull: fix Ukrainian locale

--- crowdin-pull.sh
+++ crowdin-pull.sh
@@ -3,7 +3,7 @@
 # Add new languages here, space separated and using the ID for `crowdin pull`
 LANGUAGES="es-ES de fr he hu in-context id it ja ko nl pl ru sv-SE uk zh-CN"
 
-rm -rf pages/_{es-ES,de-DE,fr-FR,he-IL,hu-HU,ic-IC,id-ID,it-IT,ja-JP,ko-KR,nl-NL,pl-PL,ru-RU,sv-SE,uk-UK,zh-CN}
+rm -rf pages/_{es-ES,de-DE,fr-FR,he-IL,hu-HU,ic-IC,id-ID,it-IT,ja-JP,ko-KR,nl-NL,pl-PL,ru-RU,sv-SE,uk-UA,zh-CN}
 
 ARG=''
 for LANGUAGE in $LANGUAGES; do
